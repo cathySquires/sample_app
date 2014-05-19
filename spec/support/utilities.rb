97c00dbb78c118914aea38ef17ca2d74a46f03e7
Cathy Squires <cathymsquires@gmail.com>
2014-05-19 16:26:48 -0400

    Generate Users and make and pass test for signup page.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,7 @@
 def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
-    "#{base_title}"
+    base_title
   else
     "#{base_title} | #{page_title}"
   end
